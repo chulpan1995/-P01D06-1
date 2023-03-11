08b7d301111ba7bf125fd2d3e559fbc005f4329c
Combine Beverly <combineb@ev-d4.kzn.21-school.ru>
2023-03-11 19:04:13 +0300

    View

--- src/pong.c
+++ src/pong.c
@@ -1,28 +1,44 @@
 #include <stdio.h>
 
-void view(int xBall, int yBall, int xLRac, int xRRac);
+void view(int xBall, int yBall, int xLRac, int xRRac, int fScore, int sScore);
 
 int main() {
+    // Стартовые позиции мяча
     int xBall = 40;
     int yBall = 12;
+    // Стартовые позиции ракеток
     int xLRac = 2;
     int xRRac = 77; 
-    view(xBall, yBall, xLRac, xRRac);
+    // Стартовый счет
+    int fScore = 0;
+    int sScore = 0;
+
+    view(xBall, yBall, xLRac, xRRac, fScore, sScore);
+
 }
 
-void view(int xBall, int yBall, int xLRac, int xRRac) {
+// Функция "Вид"
+void view(int xBall, int yBall, int xLRac, int xRRac, int fScore, int sScore) {
     int width = 80;
     int height = 25;
     for(int string = 0; string < height; string++) {
         for(int column = 0; column < width; column++) {
-          if(string == yBall)
-              if(column == xBall)
-                  printf("*");
-          else
-              printf(" ");
+            // Для первой и последней строки печатаем линию
+            if((string == 0) || (string == height - 1))
+                printf("-");
+
+            // Печатаем заполнение поля, ракетки и мяч
+            if((string == yBall) || (string == yBall + 1) || (string == yBall - 1))
+                if((column == xBall) && (string == yBall))
+                    printf("*");
+                else if(column == xLRac)
+                    printf("|");  
+                else if(column == xRRac)
+                    printf("|");      
+            else
+                printf(" ");      
         }
         printf("\n");
-    }
-    
-    
+    } 
+    printf("Score %d : %d\n", fScore, sScore);
 }
