32efca93bc333d5e6303498beb60ad5d5bcaf03d
Combine Beverly <combineb@ev-d4.kzn.21-school.ru>
2023-03-11 17:43:06 +0300

    Ball

--- src/pong.c
+++ src/pong.c
@@ -1,5 +1,28 @@
-//I WANT TO PLAY WITH YOU
-//        YOUR FRIEND, AI
+#include <stdio.h>
 
+void view(int xBall, int yBall, int xLRac, int xRRac);
 
+int main() {
+    int xBall = 40;
+    int yBall = 12;
+    int xLRac = 2;
+    int xRRac = 77; 
+    view(xBall, yBall, xLRac, xRRac);
+}
 
+void view(int xBall, int yBall, int xLRac, int xRRac) {
+    int width = 80;
+    int height = 25;
+    for(int string = 0; string < height; string++) {
+        for(int column = 0; column < width; column++) {
+          if(string == yBall)
+              if(column == xBall)
+                  printf("*");
+          else
+              printf(" ");
+        }
+        printf("\n");
+    }
+    
+    
+}
